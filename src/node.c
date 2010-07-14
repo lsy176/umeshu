d0df5c181b3a7eb72feb7b3a58837a71cd90cc57
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2010-07-14 16:58:55 +0900

    Added routines (and corresponding tests) for working with mesh nodes.

---
+++ src/node.c
@@ -0,0 +1,76 @@
+/* Copyright (c) 2010 Vladimir Chalupecky
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <math.h>
+#include <stdio.h>
+
+#include "mesh_macros.h"
+#include "node.h"
+
+Node * node_new( gdouble x, gdouble y )
+{
+    Node *node = g_slice_new( Node );
+    NODE_POSITION(node)->x = x;
+    NODE_POSITION(node)->y = y;
+    node->out_halfedge = NULL;
+    return node;
+}
+
+
+void node_free( Node *node )
+{
+    g_slice_free( Node, node );
+}
+
+
+void node_print( Node *node )
+{
+    g_return_if_fail( node != NULL );
+
+    g_print( "node %p: position = [%f,%f], out_halfedge = %p\n",
+            node, NODE_POSITION(node)->x, NODE_POSITION(node)->y, node->out_halfedge );
+}
+
+
+gboolean node_is_isolated( const Node *node )
+{
+    return node->out_halfedge == NULL;
+}
+
+
+guint node_degree( Node *node )
+{
+    g_return_val_if_fail( node != NULL, 0 );
+
+    guint degree = 0;
+    if ( ! node_is_isolated( node ) )
+    {
+        HalfEdge *current_he = node->out_halfedge;
+        do
+        {
+            degree++;
+            current_he = current_he->pair->next;
+        }
+        while ( current_he != node->out_halfedge );
+    }
+    return degree;
+}
+
---
+++ src/node.h
@@ -0,0 +1,73 @@
+/* Copyright (c) 2010 Vladimir Chalupecky
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __NODE_H_INCLUDED__
+#define __NODE_H_INCLUDED__ 
+
+/** \file
+ * \brief Node-related functions and macros
+ *
+ * \author Vladimir Chalupecky
+ */
+
+#include "mesh_types.h"
+
+/** Allocates and initializes a new, isolated node.
+ *
+ * \param x x-coordinate
+ * \param y y-coordinate
+ *
+ * \return the new node
+ */
+Node * node_new( gdouble x, gdouble y );
+
+/** Frees the node from memory.
+ *
+ * \param node node to be freed
+ */
+void node_free( Node *node );
+
+/** Prints information about the node.
+ *
+ * \param node node
+ */
+void node_print( Node *node );
+
+/** Returns a boolean indicating if the node is isolated (has no outgoing
+ * half-edge).
+ *
+ * \param node node
+ *
+ * \return TRUE if the node is isolated (no outgoing half-edge), otherwise
+ * returns FALSE
+ */
+gboolean node_is_isolated( const Node *node );
+
+/** Returns the degree (number of incident edges) of the node.
+ *
+ * \param node node
+ *
+ * \return number of incident edges
+ */
+guint node_degree( Node *node );
+
+#endif /* __NODE_H_INCLUDED__ */
+
