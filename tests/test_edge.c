36895d1e5a67f3059f20a64b2f51e96ebb2969c8
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2010-07-21 10:38:17 +0900

    Added new tests for nodes and edges.

--- tests/test_edge.c
+++ tests/test_edge.c
@@ -19,9 +19,12 @@
  * SOFTWARE.
  */
 
-#include <edge.h>
-#include <node.h>
 #include <math.h>
+
+#include <edge.h>
+#include <mesh.h>
+#include <node.h>
+#include <point2.h>
 
 int test_edge( int argc, char *argv[] )
 {
@@ -59,6 +62,43 @@ int test_edge( int argc, char *argv[] )
     edge_free( e );
     node_free( n1 );
     node_free( n2 );
+
+    Mesh *mesh = mesh_new();
+    n1 = mesh_add_node( mesh, 0.0, 0.0 );
+    n2 = mesh_add_node( mesh, 1.0, 0.0 );
+    Node *n3 = mesh_add_node( mesh, 1.0, 1.0 );
+    Node *n4 = mesh_add_node( mesh, 0.0, 1.0 );
+    Node *n5 = mesh_add_node( mesh, 0.5, 0.5 );
+
+    Edge *e1 = mesh_add_edge( mesh, n1, n2 );
+    Edge *e2 = mesh_add_edge( mesh, n2, n3 );
+    Edge *e3 = mesh_add_edge( mesh, n3, n4 );
+    Edge *e4 = mesh_add_edge( mesh, n4, n1 );
+
+    Edge *e5 = mesh_add_edge( mesh, n5, n1 );
+    Edge *e6 = mesh_add_edge( mesh, n5, n2 );
+    Edge *e7 = mesh_add_edge( mesh, n5, n3 );
+    Edge *e8 = mesh_add_edge( mesh, n5, n4 );
+
+    mesh_add_element( mesh, &e1->he[0], &e6->he[1], &e5->he[0] );
+    mesh_add_element( mesh, &e2->he[0], &e7->he[1], &e6->he[0] );
+    mesh_add_element( mesh, &e3->he[0], &e8->he[1], &e7->he[0] );
+    mesh_add_element( mesh, &e4->he[0], &e5->he[1], &e8->he[0] );
+
+    g_return_val_if_fail( ! halfedge_is_at_boundary( &e1->he[0] ), 1 );
+    g_return_val_if_fail( halfedge_is_at_boundary( &e1->he[1] ), 1 );
+    g_return_val_if_fail( edge_is_at_boundary( e1 ), 1 );
+    g_return_val_if_fail( ! edge_is_at_boundary( e5 ), 1 );
+
+    g_return_val_if_fail( ! edge_is_swappable( e1 ), 1 );
+    g_return_val_if_fail( ! edge_is_swappable( e5 ), 1 );
+    point2_set( NODE_POSITION( n5 ), NODE_POSITION( n5 )->x + SMALL_NUMBER, NODE_POSITION( n5 )->y + SMALL_NUMBER );
+    g_return_val_if_fail( edge_is_swappable( e5 ), 1 );
+    g_return_val_if_fail( ! edge_is_swappable( e6 ), 1 );
+    g_return_val_if_fail( ! edge_is_swappable( e7 ), 1 );
+    g_return_val_if_fail( edge_is_delaunay( e5 ), 1 );
+
+    mesh_free( mesh );
 
     return 0;
 }
